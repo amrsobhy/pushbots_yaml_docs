f7a5d532747622adac42859da62eea94081e7e53
unknown <mohamadomr33@gmail.com>
2017-11-13 18:44:48 +0200

    add router for the v1 and v2 docs

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.use(express.static('public'))
+
+app.get('/v1', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'v1.html'));
+})
+
+
+app.get('/v2', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'v2.html'));
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'v1.html'));
+})
 
 
 
-
-connect().use(serveStatic(__dirname+'/public')).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+app.listen(8080)
+console.log("localhost:8080")
